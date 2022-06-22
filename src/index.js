b6f849bdcf2dc5e974d673cea06853197e36e8e4
Kate Frolova <frolkate.alex@gmail.com>
2022-06-22 18:21:18 +0300

    feat: add function Int Reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+ 
+    let strN = String(n);
+    let strNReverse = '';
+
+    for(let i = strN.length - 1; i >= 0; i --)
+    {
+        strNReverse+= strN[i];
+    }
+    return Number(strNReverse);
 }
