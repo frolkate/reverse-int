4f70399d24f9f6368f70f2a469026f3cb846e37f
Kate Frolova <frolkate.alex@gmail.com>
2022-06-22 18:30:55 +0300

    fix: add Math.abs Int Reverse

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function reverse (n) {
  
-    let strN = String(n);
+    let strN = String(Math.abs(n));
     let strNReverse = '';
 
     for(let i = strN.length - 1; i >= 0; i --)
     {
         strNReverse+= strN[i];
     }
-    return  Math.abs(Number(strNReverse));
+    return Number(strNReverse);
 }
